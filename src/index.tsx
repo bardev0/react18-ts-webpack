22fba7db534b65a8c8f118f9500c8a43bcb3b523
GregD <test@gmail.com>
2022-12-26 14:41:30 +0100

    added working SASS support, compiler and loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 import App from './App';
+import './styles/main.scss'
 
 const root = ReactDOMClient.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
